ea43c6419a8423ff6d71905e68437ce60aaf7423
rock <zengqingcong@derbysoft.com>
2013-05-18 14:58:10 +0800

    init

---
+++ application/config/email.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+$config['mailtype']     = 'html';
+$config['wordwrap']     = true;
+
+
+
+// user registration
+$config['register_from_email']     = 'admin@fengcaixiu.com';
+$config['register_from_name']     = 'Fengcaixiu';
+$config['register_email_subject']     = 'Thankyou for registering with us';
+
+
+
+/* End of file email.php */
+/* Location: ./application/config/email.php */
